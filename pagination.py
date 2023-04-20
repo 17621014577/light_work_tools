7bda4895ff8371af63e6189c36cc99781fdf33cb
Mason <33174335+17621014577@users.noreply.github.com>
2023-04-20 08:09:52 +0000

    use arrow

--- pagination.py
+++ pagination.py
@@ -1,4 +1,5 @@
 import datetime
+import arrow
 
 
 def num_pagination(init, stop, limit=9):
@@ -9,13 +10,12 @@ def num_pagination(init, stop, limit=9):
 
 
 def day_pagination(init: str, stop: str, limit=9):
-    init = datetime.datetime.strptime(init[:10], '%Y-%m-%d')
-    _stop = stop = datetime.datetime.strptime(stop[:10], '%Y-%m-%d')
-    limit = datetime.timedelta(days=limit)
+    init = arrow.get(init, 'YYYY-MM-DD')
+    _stop = stop = arrow.get(stop, 'YYYY-MM-DD')
     while stop >= init:
-        _stop = init + limit
-        yield str(init), min([_stop, stop])
-        init = _stop + datetime.timedelta(days=1)
+        _stop = init.shift(days=limit)
+        yield init.date().__str__(), min([_stop.date().__str__(), stop.date().__str__()])
+        init = _stop.shift(days=1)
 
 
 def minute_pagination(init: str, stop: str, limit_minute=10):
@@ -25,3 +25,6 @@ def minute_pagination(init: str, stop: str, limit_minute=10):
         _stop = init + datetime.timedelta(minutes=limit_minute)
         yield str(init), min([_stop, stop])
         init = _stop + datetime.timedelta(seconds=1)
+
+for s,e in day_pagination('2023-04-20 16:06:43', '2023-07-20 16:06:53'):
+    print(s, e)
